a4a79bb181986c079a70e22e670a905a2a62dc8d
HargunM_ <158788431+CreativeCodur@users.noreply.github.com>
2025-07-15 22:58:28 -0400

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', minHeight: '100vh', fontSize: 24 }}>
+      <p>Hello from your DeveLove template!</p>
+    </div>
+  );
+}
